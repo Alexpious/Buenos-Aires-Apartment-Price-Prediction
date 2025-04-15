bf6528a105827215935b0434c11705c6e5d437a3
sodiqlef <kehindeogundimu731@gmail.com>
2025-04-15 14:18:44 +0100

    adding ignore file

--- buenosaires/buenosaires/settings.py
+++ buenosaires/buenosaires/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from sk import SECRET_KEY
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0$20i)+9*%xux2+3r8sg5=*u=@=1n@%g^_ibwlf)fqdi)f1jt2'
+SECRET_KEY = SECRET_KEY
  
 
 # SECURITY WARNING: don't run with debug turned on in production!
